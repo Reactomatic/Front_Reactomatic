38f08a6477d0d08a95dac03eede33117108ec3e8
fabien382 <fabien.nvn@gmail.com>
2024-08-22 09:33:02 +0200

    Start the profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Profile } from "@/components/component/profile";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Profile />
+    </div>
+
+  );
+}
