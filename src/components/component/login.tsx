e79ca329a91f1249d8ee28d1e90c3e3db075ec20
MolinaRomain <molinaromain04@gmail.com>
2024-08-24 10:52:49 +0200

    put the file like it was before my changes

--- src/components/component/login.tsx
+++ src/components/component/login.tsx
@@ -8,7 +8,10 @@
 
 import { Inter } from 'next/font/google'
 
-
+inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 To read more about using these font, please visit the Next.js documentation:
 - App Directory: https://nextjs.org/docs/app/building-your-application/optimizing/fonts
