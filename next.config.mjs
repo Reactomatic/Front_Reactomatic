a4a602e56f959bc2c2ca479b45e22540c58e9519
MolinaRomain <molinaromain04@gmail.com>
2024-08-24 10:45:54 +0200

    CHanged next config to update linter rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+      // Ignore TypeScript errors in production builds
+      ignoreBuildErrors: true,
+    },
     eslint: {
+      // Ignore ESLint errors in production builds
       ignoreDuringBuilds: true,
     },
   };
