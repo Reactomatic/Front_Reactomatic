2b1064464ed3d6c1d2e05f983eaf95f6115f7633
Senouci Moulay <105427812+SenouciMoulay@users.noreply.github.com>
2024-09-01 20:57:42 +0200

    feature/sidebar
    last adjustment

--- src/app/configuration/page.tsx
+++ src/app/configuration/page.tsx
@@ -5,7 +5,7 @@ import { ComponentListCreation } from "@/components/component/component/componen
 export default function ConfigurationPage() {
   return (
     <ConfigProvider>
-      <div className="container mt-19 mx-auto h-full">
+      <div className="container mt-20 mx-auto h-full">
         <div className="flex flex-col h-full md:flex-row gap-6">
           <div className="w-full md:w-2/3">
             <ComponentListCreation />
