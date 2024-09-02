dc2d148db859d8726dbe15ada49e3dbab8e68d06
Romain Molina <romain.molina@getyooz.com>
2024-09-02 18:03:47 +0200

    dotenv

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000', // Base URL de votre backend
