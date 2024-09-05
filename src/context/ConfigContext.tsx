dbad59865d0ce57140f7fa865ef84cfd17cad407
Senouci Moulay <105427812+SenouciMoulay@users.noreply.github.com>
2024-09-05 21:49:16 +0200

    release
    new type implemented, juste have to plug backend

--- src/context/ConfigContext.tsx
+++ src/context/ConfigContext.tsx
@@ -3,9 +3,6 @@
 import React, { createContext, useState, useContext } from 'react';
 import {ComponentData, ComponentType, Config, ConfigContextType} from "@/components/component/component/admin/types";
 
-// Alias pour rendre PCComponentType et ComponentType compatibles
-type PCComponentType = ComponentType; // Ajoutez cet alias si nécessaire
-
 const ConfigContext = createContext<ConfigContextType | undefined>(undefined);
 
 export const ConfigProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -17,12 +14,6 @@ export const ConfigProvider: React.FC<{ children: React.ReactNode }> = ({ childr
 
     const getTotalPrice = () => {
         return Object.values(config).reduce((total, component) => total + (component?.price || 0), 0);
-    };
-
-    type ConfigContextType = {
-        config: Config;
-        setComponentSelection: (type: ComponentType, component: ComponentData | undefined) => void;
-        getTotalPrice: () => number;
     };
 
     return (
