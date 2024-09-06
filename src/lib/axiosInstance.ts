a9c0751f4e6a5b6e393ebd8fab46bd5308f0ef2d
Senouci Moulay <105427812+SenouciMoulay@users.noreply.github.com>
2024-09-06 22:05:19 +0200

    release
    card in div scrollable
    title for configuration

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 // import useAuthStore from '@/stores/useAuthStore';
-=======
 // import * as dotenv from 'dotenv';
-
 // dotenv.config();
 
 console.log('Base URL:', process.env.NEXT_PUBLIC_API_BASE_URL); // Vérifiez si l'URL est bien chargée
