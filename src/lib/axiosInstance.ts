f81579c58354288d28f2622b170f0da19d78ae4e
fabien382 <fabien.nvn@gmail.com>
2024-09-06 20:33:43 +0200

    CONNECT CRUD COMPONENT

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,10 +1,31 @@
 import axios from 'axios';
 import * as dotenv from 'dotenv';
+// import useAuthStore from '@/stores/useAuthStore';
 
 dotenv.config();
 
+// Crée une instance d'axios
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000', // Base URL de votre backend
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
 });
+
+// Interceptor pour ajouter le Bearer token
+axiosInstance.interceptors.request.use(
+  
+  (config) => {
+    // const { isAdmin, isAuthenticated } = useAuthStore.getState() as { isAdmin: string, isAuthenticated: boolean };
+
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
